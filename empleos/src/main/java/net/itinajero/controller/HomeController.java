610850333e72c0f57b792b3de3a5390d061d0f2a
musiciancoder <andresguitarra1980@gmail.com>
2020-03-24 16:00:54 -0300

    Agregar tipos de datos simples al modelo y desplegarlos en la vista 219

--- empleos/src/main/java/net/itinajero/controller/HomeController.java
+++ empleos/src/main/java/net/itinajero/controller/HomeController.java
@@ -12,8 +12,22 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String mostrarHome(Model model) {
-		model.addAttribute("mensaje", "Bienvenido a nuestra app");//al igual q en springMVS "mensaje" va a la vista thymeleaf y "Bienvenido..." es un objeto (en este caso un string)
-		model.addAttribute("fecha", new Date()); //en este caso el objeto es una fecha
+		/*
+		 * model.addAttribute("mensaje", "Bienvenido a nuestra app");//al igual q en
+		 * springMVS "mensaje" va a la vista thymeleaf y "Bienvenido..." es un objeto
+		 * (en este caso un string) model.addAttribute("fecha", new Date()); //en este
+		 * caso el objeto es una fecha
+		 */		
+		
+		String titulo = "Auxiliar de contabilidad";
+		Date fecha = new Date();
+		Double salario = 9999.0;
+		Boolean vigente = true;
+		
+		model.addAttribute("titulo", titulo );
+		model.addAttribute("fecha", fecha );
+		model.addAttribute("salario", salario );
+		model.addAttribute("vigente", vigente);
 		return "home"; //nunca se necesita la extension
 	}
 
