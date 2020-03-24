4f83d1aa1e41c1223533b13d6a2419439f9636e0
musiciancoder <andresguitarra1980@gmail.com>
2020-03-24 15:11:54 -0300

    216 Agregar informacion al modelo para desplegarla en la vista

--- empleos/src/main/java/net/itinajero/controller/HomeController.java
+++ empleos/src/main/java/net/itinajero/controller/HomeController.java
@@ -1,6 +1,9 @@
 package net.itinajero.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,7 +11,9 @@ public class HomeController {
 	
 	
 	@GetMapping("/")
-	public String mostrarHome() {
+	public String mostrarHome(Model model) {
+		model.addAttribute("mensaje", "Bienvenido a nuestra app");//al igual q en springMVS "mensaje" va a la vista thymeleaf y "Bienvenido..." es un objeto (en este caso un string)
+		model.addAttribute("fecha", new Date()); //en este caso el objeto es una fecha
 		return "home"; //nunca se necesita la extension
 	}
 
