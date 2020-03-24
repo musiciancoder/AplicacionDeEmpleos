129aa9bd46055f5c52b9e5d0ddf9233040bb7dfa
musiciancoder <andresguitarra1980@gmail.com>
2020-03-24 16:32:05 -0300

    219.219. Iteraciones en Thymeleaf - Expresión <th:each>

--- empleos/src/main/java/net/itinajero/controller/HomeController.java
+++ empleos/src/main/java/net/itinajero/controller/HomeController.java
@@ -1,6 +1,8 @@
 package net.itinajero.controller;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+	
+	@GetMapping("/listado")
+	public String mostrarListado(Model model) {
+		List<String>lista = new LinkedList<String>();
+		lista.add("Ingeniero de sistemas");
+		lista.add("Auxiliar contabilidad");
+		lista.add("Vendedor");
+		lista.add("Arquitecto");
+		model.addAttribute("empleos", lista);
+		return "listado"; //en spring en el controlado se puede retornar un objeto (html) que no coincide con el tipo definido inicialmente en el metodo
+		
+	}
 	
 	
 	@GetMapping("/")
@@ -28,7 +42,8 @@ public class HomeController {
 		model.addAttribute("fecha", fecha );
 		model.addAttribute("salario", salario );
 		model.addAttribute("vigente", vigente);
-		return "home"; //nunca se necesita la extension
+		return "home"; //nunca se necesita la extension. en spring en el controlado se puede retornar un objeto (html) que no coincide con el tipo definido inicialmente en el metodo
+		
 	}
 
 	
