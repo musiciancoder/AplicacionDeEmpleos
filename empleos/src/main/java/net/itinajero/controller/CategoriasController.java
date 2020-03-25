ac5fa689ee19572050a19b46ee2e4273ae41342c
musiciancoder <andresguitarra1980@gmail.com>
2020-03-25 17:11:01 -0300

    233 PathVariable

--- empleos/src/main/java/net/itinajero/controller/CategoriasController.java
+++ empleos/src/main/java/net/itinajero/controller/CategoriasController.java
@@ -2,6 +2,7 @@ package net.itinajero.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/categorias")
 public class CategoriasController {
 
-	// @GetMapping("/index")
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	 @GetMapping("/index")
+	//@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String mostrarIndex(Model model) {
 		return "categorias/listCategorias";
 	}
---
+++ empleos/src/main/java/net/itinajero/controller/VacantesController.java
@@ -0,0 +1,26 @@
+package net.itinajero.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/vacantes")
+public class VacantesController {
+	
+	@GetMapping("/view/{id}")
+	public String verDetalle(@PathVariable("id") int idVacante, Model model) {
+		System.out.println("IdVacante: " + idVacante);
+		model.addAttribute("idVacante", idVacante);
+		
+		//TODO Buscar los datos de la vacante en la BBDD
+		
+		return "vacantes/detalle";
+		
+	}
+
+}
