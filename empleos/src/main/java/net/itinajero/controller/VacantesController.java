90a1af6cce6a726ce01276b42f67749809b8b9a2
musiciancoder <andresguitarra1980@gmail.com>
2020-03-25 19:31:34 -0300

    236 ejemplo de peticion get con RequestParam

--- empleos/src/main/java/net/itinajero/controller/VacantesController.java
+++ empleos/src/main/java/net/itinajero/controller/VacantesController.java
@@ -5,22 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/vacantes")
 public class VacantesController {
-	
-	@GetMapping("/view/{id}")
-	public String verDetalle(@PathVariable("id") int idVacante, Model model) {
+
+	@GetMapping("/delete") //para llamar con vacantes/delete?id=3, por ejemplo. Notese que el nombre del atributo Si viaja junto al valor de este. Al pasar el mouse por encima del link SI se ve la url a la que nos dirigira si lo presionamos
+	public String eliminar(@RequestParam("id") int idVacante, Model model) { //@RequestParam es mas apropiado para manipular bases de datos a traves de botones en nuestras vistas html
+		System.out.println("Eliminando vacante con id :" + idVacante);
+		model.addAttribute("id", idVacante);
+		return "mensaje";
+	}
+
+	@GetMapping("/view/{id}") //para llamar con vacantes/view/3, por ejemplo. Notese que el nombre del atributo NO viaja, solo el valor de este. Al pasar el mouse por encima del link NO se ve nada
+	public String verDetalle(@PathVariable("id") int idVacante, Model model) { //@PathVariable es mas apropiado para expandir y renderizar detalles de un objeto
 		System.out.println("IdVacante: " + idVacante);
 		model.addAttribute("idVacante", idVacante);
-		
-		//TODO Buscar los datos de la vacante en la BBDD
-		
+
+		// TODO Buscar los datos de la vacante en la BBDD
+
 		return "vacantes/detalle";
-		
+
 	}
 
 }
