8754747da0f5f77cd08347dc8e9f0dfc309fd513
musiciancoder <andresguitarra1980@gmail.com>
2020-04-01 14:58:44 -0300

    Agregar caracteres aleatorios al nombre del archivo para evitar duplicados66

--- empleos/src/main/java/net/itinajero/util/Utileria.java
+++ empleos/src/main/java/net/itinajero/util/Utileria.java
@@ -10,16 +10,38 @@ public class Utileria {
 public static String guardarArchivo(MultipartFile multiPart, String ruta) {
 // Obtenemos el nombre original del archivo.
 String nombreOriginal = multiPart.getOriginalFilename();
+nombreOriginal= nombreOriginal.replace(" ", "-");
+String nombreFinal = randomAlphaNumeric(8) + nombreOriginal;
 try {
 // Formamos el nombre del archivo para guardarlo en el disco duro.
-File imageFile = new File(ruta + nombreOriginal);
+File imageFile = new File(ruta + nombreFinal);
 System.out.println("Archivo: " + imageFile.getAbsolutePath());
 //Guardamos fisicamente el archivo en HD.
 multiPart.transferTo(imageFile);
-return nombreOriginal;
+return nombreFinal;
 } catch (IOException e) {
 System.out.println("Error " + e.getMessage());
 return null;
 }
 }
+
+
+/**
+ * Metodo para generar una cadena aleatoria de longitud N
+ * @param count
+ * @return
+ */
+public static String randomAlphaNumeric(int count) {
+	String CARACTERES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	StringBuilder builder = new StringBuilder();
+	while (count-- != 0) {
+		int character = (int) (Math.random() * CARACTERES.length());
+		builder.append(CARACTERES.charAt(character));
+	}
+	return builder.toString();
 }
+
+}
+
+
+
