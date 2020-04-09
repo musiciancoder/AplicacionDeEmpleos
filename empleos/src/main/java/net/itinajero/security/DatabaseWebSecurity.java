1e0152c834d07b523e3570969a67e175592d3dda
musiciancoder <andresguitarra1980@gmail.com>
2020-04-09 14:27:29 -0400

    Personalizar accesos a URLs

--- empleos/src/main/java/net/itinajero/security/DatabaseWebSecurity.java
+++ empleos/src/main/java/net/itinajero/security/DatabaseWebSecurity.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -14,16 +15,32 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
 	@Autowired
-	private DataSource dataSource; //DataSource es la bbdd mysql empleosdb que tenemos configurada en archivo aplications.properties
+	private DataSource dataSource; // DataSource es la bbdd mysql empleosdb que tenemos configurada en archivo
+									// aplications.properties
 
-	//Los usuarios los saca de la tabla usuarios y los authorities (roles) de la tabla intermedia usuarioperfil
+	// Los usuarios los saca de la tabla usuarios y los authorities (roles) de la
+	// tabla intermedia usuarioperfil
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource).
-		usersByUsernameQuery("select username, password, estatus from Usuarios where username=?")
-		.authoritiesByUsernameQuery("select u.username, p.perfil from UsuarioPerfil up " +
-		"inner join Usuarios u on u.id = up.idUsuario " +
-		"inner join Perfiles p on p.id = up.idPerfil " +
-		"where u.username = ?");
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, estatus from Usuarios where username=?")
+				.authoritiesByUsernameQuery("select u.username, p.perfil from UsuarioPerfil up "
+						+ "inner join Usuarios u on u.id = up.idUsuario "
+						+ "inner join Perfiles p on p.id = up.idPerfil " + "where u.username = ?");
 	}
+
+	// Personalizar accesos a URLs
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				// Los recursos estáticos no requieren autenticación
+				.antMatchers("/bootstrap/**", "/images/**", "/tinymce/**", "/logos/**").permitAll()
+				// Las vistas públicas no requieren autenticación
+				.antMatchers("/", "/signup", "/search", "/vacantes/view/**").permitAll()
+				// Todas las demás URLs de la Aplicación requieren autenticación
+				.anyRequest().authenticated()
+				// El formulario de Login no requiere autenticacion
+				.and().formLogin().permitAll();
+	}
+
 }
